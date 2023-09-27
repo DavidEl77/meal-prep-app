edc8027dc9c18287cf4d9b06c20ffc40ae96b7e7
DavidEl77 <112475152+DavidEl77@users.noreply.github.com>
2023-09-27 20:59:32 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import MealCreator from "./pages/MealCreator";
+
+function App() {
+  return <MealCreator />;
+}
+
+export default App;
---
+++ src/pages/MealCreator.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MealCreator = () => {
+  return (
+    <div>
+      <div>Welcome to the ,,,</div>;
+      <input type="text" />
+    </div>
+  );
+};
+
+export default MealCreator;
