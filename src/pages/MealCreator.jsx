c11aa94e812a6e5b4a1d08d54c809c2b0c810b05
DavidEl77 <112475152+DavidEl77@users.noreply.github.com>
2023-09-27 21:06:29 +0300

    Minor

--- src/pages/MealCreator.jsx
+++ src/pages/MealCreator.jsx
@@ -3,7 +3,8 @@ import React from "react";
 const MealCreator = () => {
   return (
     <div>
-      <div>Welcome to the ,,,</div>;
+      <div>Welcome to the ,,,</div>
+      <input type="text" />
       <input type="text" />
     </div>
   );
